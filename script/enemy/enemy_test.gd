1546f9e0edb15fe45bd349a4c384f0c31dd8d532
applehoro <apple.horo@gmail.com>
2024-12-05 13:13:40 +0600

    reworked test enemy

--- script/enemy/enemy_test.gd
+++ script/enemy/enemy_test.gd
@@ -1,22 +1,46 @@
 extends CharacterBody3D
 
-var motion = Vector3();
+@export var max_health = 40.0;
+@onready var health = max_health;
 
+@export_category( "Cooldown" )
+@export var idle_cooldown = Vector2( 0.1, 0.4 );
+@export var chase_cooldown = Vector2( 0.5, 0.9 );
+@export var dodge_cooldown = Vector2( 0.3, 0.6 );
+@export var block_cooldown = Vector2( 0.4, 0.7 );
+@export var flinch_cooldown = Vector2( 0.6, 0.7 );
+
+@export_category( "Flinch" )
+@export var flinch_chance = 50;
+
+@export_category( "Dodge" )
 var motion_dodge = Vector3();
 var dodge_cd = 0.0;
-var can_dodge = true;
-var dodge_vel = 32.0;
+@export var can_dodge = true;
+@export var dodge_vel = 32.0;
+@export var dodge_chance = 15;
+@export var dodge_delay = Vector2( 6.0, 9.0 );
+@export var dodge_damp = 6.0;
 
+@export_category( "Block" )
 var block_cd = 0.0;
-var can_block = false;
+@export var can_block = true;
+@export var block_chance = 15;
+@export var block_delay = Vector2( 8.0, 12.0 );
+@export var block_mul = 0.25;
 
 var is_grounded = true;
 
-var move_vel = 24.0;
+@export_category( "Motion" )
+var motion = Vector3();
+@export var move_vel = 24.0;
+@export var motion_damp = 8.0;
+@export var move_offset_angle = 45.0;
 var move_offset = 0.0;
+var push_vel = Vector3();
+@export var push_mul = 0.05;
 
 var targeted_cd = 0.0;
-
 var look_pos = Vector2();
 
 var ai_state = "idle";
@@ -31,15 +55,16 @@ var attack_list = {};
 var attack_cd = 0.0;
 
 func _ready() -> void:
-	look_at_pos( global_position - $yaw.global_basis.z );
+	look_at_pos( global_position - global_basis.z );
 	$radar_mark.visible = true;
 	set_ai_state( ai_state );
 
 func _physics_process(delta: float) -> void:
 	
 	# motion damping
-	motion *= 1.0 - delta*8.0;
-	motion_dodge *= 1.0 - delta*1.5;
+	motion *= 1.0 - delta*motion_damp;
+	push_vel *= 1.0 - delta*motion_damp;
+	motion_dodge *= 1.0 - delta*dodge_damp;
 	
 	# cooldowns
 	targeted_cd = max( targeted_cd - delta,  0.0 );
@@ -58,6 +83,7 @@ func _physics_process(delta: float) -> void:
 			motion.y = 0.0;
 		else:
 			motion.y = min( motion.y - delta*120.0, 0.0 );
+	motion += push_vel;
 	velocity = motion;
 	move_and_slide();
 
@@ -65,13 +91,13 @@ func check_player():
 	if( Global.node_player != null ):
 		var offset = Global.player_pos - global_position;
 		player_dist = global_position.distance_to( Global.player_pos );
-		player_yaw_angle = $yaw.global_basis.z.signed_angle_to( -offset, $yaw.global_basis.y );
-		player_pitch_angle = $yaw/pitch.global_basis.z.signed_angle_to( -offset, $yaw/pitch.global_basis.x );
+		player_yaw_angle = global_basis.z.signed_angle_to( -offset, global_basis.y );
+		player_pitch_angle = $pitch.global_basis.z.signed_angle_to( -offset, $pitch.global_basis.x );
 		
 		if( player_dist < 120.0 && abs( player_yaw_angle ) < PI/2.0 && abs( player_pitch_angle ) < PI/2.0 ):
 			for xi in range( -1, 1 ):
 				for yi in range( -1, 1 ):
-					var p = Global.player_pos + $yaw/pitch.global_basis.x*xi + $yaw/pitch.global_basis.y*yi;
+					var p = Global.player_pos + $pitch.global_basis.x*xi + $pitch.global_basis.y*yi;
 					var r = Global.raycast_3d_body( global_position, p, [ self, Global.node_player ] );
 					if( !r ):
 						saw_player = true;
@@ -87,38 +113,39 @@ func is_targeted():
 func set_look( v ):
 	look_pos = v;
 	look_pos.y = clamp( look_pos.y, -PI/2.0, PI/2.0 );
-	$yaw.rotation.y = look_pos.x;
-	$yaw/pitch.rotation.x = look_pos.y;
+	rotation.y = look_pos.x;
+	$pitch.rotation.x = look_pos.y;
 
 func look_at_pos( pos ):
 	var offset = pos - global_position;
-	var hz_a = $yaw.global_basis.z.signed_angle_to( -offset, $yaw.global_basis.y );
-	var vt_a = $yaw/pitch.global_basis.z.signed_angle_to( -offset, $yaw/pitch.global_basis.x );
+	var hz_a = global_basis.z.signed_angle_to( -offset, global_basis.y );
+	var vt_a = $pitch.global_basis.z.signed_angle_to( -offset, $pitch.global_basis.x );
 	set_look( look_pos + Vector2( hz_a, vt_a ) );
 
 func slow_look_at_pos( pos, delta ):
 	var offset = pos - global_position;
-	var hz_a = $yaw.global_basis.z.signed_angle_to( -offset, $yaw.global_basis.y );
-	var vt_a = $yaw/pitch.global_basis.z.signed_angle_to( -offset, $yaw/pitch.global_basis.x );
+	var hz_a = global_basis.z.signed_angle_to( -offset, global_basis.y );
+	var vt_a = $pitch.global_basis.z.signed_angle_to( -offset, $pitch.global_basis.x );
 	
 	if( abs( hz_a ) > 0.05 ):
 		look_pos.x = lerp( look_pos.x, look_pos.x + hz_a, delta + abs( hz_a )/10.0 );
-		$yaw.rotation.y = look_pos.x;
+		rotation.y = look_pos.x;
 	else:
-		$yaw.look_at( pos, $yaw.global_basis.y );
+		look_at( pos, global_basis.y );
 	
 	if( abs( vt_a ) > 0.05 ):
 		look_pos.y = lerp( look_pos.y, look_pos.y + vt_a, delta + abs( vt_a )/10.0 );
 		look_pos.y = clamp( look_pos.y, -PI/2.0, PI/2.0 );
-		$yaw/pitch.rotation.x = look_pos.y;
+		$pitch.rotation.x = look_pos.y;
 	else:
-		$yaw/pitch.look_at( pos, $yaw.global_basis.y );
+		$pitch.look_at( pos, global_basis.y );
 
-func register_attack( id, dist_range, cd_range, anim_name ):
+func register_attack( id, dist_range: Vector2, cd_range: Vector2, anim_name, chance ):
 	attack_list[ id ] = {
 		"dist_range": dist_range,
 		"cd_range": cd_range,
 		"anim_name": anim_name,
+		"chance": chance,
 	};
 
 func can_attack():
@@ -130,10 +157,10 @@ func ai_state_cooldown( delta ):
 		if( can_attack() && saw_player && randi()%100 < 50 ):
 			set_ai_state( "attack" );
 		
-		elif( is_targeted() && block_cd <= 0.0 && saw_player && can_block && randi()%100 < 15 ):
+		elif( is_targeted() && block_cd <= 0.0 && saw_player && can_block && randi()%100 < dodge_chance ):
 			set_ai_state( "block" );
 		
-		elif( is_targeted() && dodge_cd <= 0.0 && saw_player && can_dodge && randi()%100 < 15 ):
+		elif( is_targeted() && dodge_cd <= 0.0 && saw_player && can_dodge && randi()%100 < block_chance ):
 			set_ai_state( "dodge" );
 		
 		elif( saw_player ):
@@ -151,10 +178,10 @@ func ai_state_update( delta ):
 		slow_look_at_pos( Global.player_pos, delta*2.0 );
 		var offset = Global.player_pos - global_position;
 		player_dist = global_position.distance_to( Global.player_pos );
-		player_yaw_angle = $yaw.global_basis.z.signed_angle_to( -offset, $yaw.global_basis.y );
-		player_pitch_angle = $yaw/pitch.global_basis.z.signed_angle_to( -offset, $yaw/pitch.global_basis.x );
+		player_yaw_angle = global_basis.z.signed_angle_to( -offset, global_basis.y );
+		player_pitch_angle = $pitch.global_basis.z.signed_angle_to( -offset, $pitch.global_basis.x );
 		if( player_dist > 4.0 ):
-			motion -= $yaw/pitch.global_basis.z.rotated( Vector3.UP, deg_to_rad( move_offset*40.0 ) )*move_vel*delta;
+			motion -= $pitch.global_basis.z.rotated( Vector3.UP, deg_to_rad( move_offset*move_offset_angle ) )*move_vel*delta;
 	
 	elif( ai_state == "dodge" ):
 		slow_look_at_pos( Global.player_pos, delta/1.5 );
@@ -166,11 +193,12 @@ func set_ai_state( s ):
 	ai_state = s;
 	match( ai_state ):
 		"idle":
-			ai_state_cd = randf_range( 0.1, 0.4 );
+			ai_state_cd = randf_range( idle_cooldown.x, idle_cooldown.y );
 		
 		"chase":
-			ai_state_cd = randf_range( 0.5, 0.9 );
+			ai_state_cd = randf_range( idle_cooldown.x, idle_cooldown.y );
 			move_offset = randf_range( -1.0, 1.0 );
+			saw_player = true;
 		
 		"attack":
 			var i = randi()%attack_list.keys().size();
@@ -179,15 +207,38 @@ func set_ai_state( s ):
 			ai_state_cd = attack_cd;
 		
 		"block":
-			ai_state_cd = randf_range( 0.4, 0.7 );
-			block_cd = randf_range( 8.0, 12.0 );
+			ai_state_cd = randf_range( block_cooldown.x, block_cooldown.y );
+			block_cd = randf_range( block_delay.x, block_delay.y );
 		
 		"dodge":
-			ai_state_cd = randf_range( 0.3, 0.6 );
-			var dir = $yaw.global_basis.x;
+			ai_state_cd = randf_range( dodge_cooldown.x, dodge_cooldown.y );
+			dodge_cd = randf_range( dodge_delay.x, dodge_delay.y );
+			
+			var dir = global_basis.x;
 			if( randi()%100 > 75 ):
-				dir = $yaw.global_basis.y;
+				dir = global_basis.y;
 			if( randi()%100 > 50 ):
 				dir *= -1.0;
 			motion_dodge = dir*dodge_vel;
-			dodge_cd = randf_range( 6.0, 9.0 );
+		
+		"flinch":
+			ai_state_cd = randf_range( flinch_cooldown.x, flinch_cooldown.y );
+			saw_player = true;
+
+
+func push( v ):
+	push_vel += v*push_mul;
+
+func damage( d ):
+	if( block_cd > 0.0 ):
+		health -= d*block_mul;
+	else:
+		health -= d;
+	
+	if( health <= 0.0 ):
+		die();
+	elif( randi()%100 < flinch_chance ):
+		set_ai_state( "flinch" );
+
+func die():
+	queue_free();
